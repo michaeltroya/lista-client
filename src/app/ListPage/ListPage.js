f37f9f4a5af72cae8b33551a350cda4fd49a8151
Michael <michaeltroya13@gmail.com>
2020-02-27 12:00:13 -0500

    setup expanded list page

--- src/app/ListPage/ListPage.js
+++ src/app/ListPage/ListPage.js
@@ -1,6 +1,60 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+//gql
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+//bs imports
+import { Container, Spinner } from 'react-bootstrap';
+//comps
+import List from '../../components/ListCard/ListCard';
+import Nav from '../../components/Nav/Nav';
 
-const ListPage = () => {
-  return <div>hiii</div>;
+const ListPage = props => {
+  const listIdPath = props.location.pathname.split('/')[3];
+
+  const { loading, data } = useQuery(FETCH_LIST_QUERY, {
+    variables: {
+      listId: listIdPath
+    }
+  });
+
+  console.log(data);
+
+  return (
+    <Fragment>
+      <Nav />
+      <div className="profile">
+        <Container>
+          {loading ? (
+            <Spinner animation="border" className="orange-spinner" />
+          ) : (
+            <List list={data.getList} key={data.getList.id} />
+          )}
+        </Container>
+      </div>
+    </Fragment>
+  );
 };
+
+const FETCH_LIST_QUERY = gql`
+  query List($listId: ID!) {
+    getList(listId: $listId) {
+      id
+      username
+      title {
+        phrase
+        count
+        description
+      }
+      tags
+      items {
+        name
+        description
+        order
+      }
+      commentCount
+      likeCount
+    }
+  }
+`;
+
 export default ListPage;
