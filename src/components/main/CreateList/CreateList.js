9adbffc647bd6b356ce8a7a3f9bbef5e823cae47
Michael <michaeltroya13@gmail.com>
2020-03-07 19:03:06 -0500

    setup dynamic inputs based on user input

--- src/components/main/CreateList/CreateList.js
+++ src/components/main/CreateList/CreateList.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 //gql
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_LIST } from '../../../graphql/mutations';
@@ -7,7 +7,8 @@ import Nav from '../../layout/Nav/Nav';
 import { Container } from 'react-bootstrap';
 
 const CreateList = props => {
-  const [itemCount, setItemCount] = useState(0);
+  const [itemCount, setItemCount] = useState(3);
+  const [items, setItems] = useState([]);
 
   const [createList, { loading }] = useMutation(CREATE_LIST, {
     variables: {
@@ -23,22 +24,45 @@ const CreateList = props => {
 
   const handleTitleSubmit = e => {
     e.preventDefault();
+
     console.log(itemCount);
+    for (let i = 0; i < itemCount; i++) {
+      const item = {
+        order: i + 1,
+        name: '',
+        description: ''
+      };
+
+      setItems([...items, item]);
+    }
+
+    console.log(items);
+  };
+
+  const handleItemsSubmit = e => {
+    e.preventDefault();
   };
 
   return (
     <Fragment>
       <Nav type="compose" history={props.history} />
-      <Container>
-        {itemCount === 0 ? (
+      <div className="create-list-title">
+        <Container>
           <form onSubmit={handleTitleSubmit}>
             <h3>Title</h3>
-            <input type="number" placeholder="Amount" />
+            <input
+              type="number"
+              placeholder="Amount"
+              value={itemCount}
+              onChange={e => setItemCount(e.target.value)}
+              min="3"
+              max="100"
+            />
             <input type="text" placeholder="Description" />
             <input type="submit" className="btn" />
           </form>
-        ) : null}
-      </Container>
+        </Container>
+      </div>
     </Fragment>
   );
 };
