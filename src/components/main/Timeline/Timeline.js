4c4b96a8b82b36458eab63fca90ded1431911835
Michael <michaeltroya13@gmail.com>
2020-03-11 13:27:27 -0400

    display treding and friends lists on timeline

--- src/components/main/Timeline/Timeline.js
+++ src/components/main/Timeline/Timeline.js
@@ -8,19 +8,34 @@ import Nav from '../../layout/Nav/Nav';
 //comps
 import ListCard from '../../secondary/ListCard/ListCard';
 //queries
-import { FETCH_TIMELINE_QUERY } from '../../../graphql/query';
+import { FETCH_TIMELINE_QUERY, FETCH_ALL_LISTS_QUERY } from '../../../graphql/query';
 //bs imports
 import { Spinner, Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
   const following = useSelector(state => state.user.following);
   const { loading, data } = useQuery(FETCH_TIMELINE_QUERY, { variables: { following } });
+  const { loading: trendLoad, data: trendData } = useQuery(FETCH_ALL_LISTS_QUERY);
 
   return (
     <Fragment>
       <Nav type="timeline" />
-      <section className="home-timeline">
+      <section className="trending-timeline">
         <Container>
+          <h1>Trending Lists</h1>
+
+          <div className="lists-container">
+            {trendLoad ? (
+              <Spinner animation="border" className="orange-spinner" />
+            ) : (
+              trendData.getLists.map(list => <ListCard list={list} />)
+            )}
+          </div>
+        </Container>
+      </section>
+      <section className="friends-timeline">
+        <Container>
+          <h1>My friends Lists</h1>
           <Row>
             {loading ? (
               <Spinner animation="border" className="orange-spinner" />
